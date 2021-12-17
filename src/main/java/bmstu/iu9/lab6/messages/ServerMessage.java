3dff43b78dbad3deea152f2b6287e020035fca90
zyxbmstu <zaxarler@mail.ru>
2021-12-17 11:42:36 +0300

    Scripted auto-commit on change (2021-12-17 11:42:36) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab6/messages/ServerMessage.java
+++ src/main/java/bmstu/iu9/lab6/messages/ServerMessage.java
@@ -1,4 +1,15 @@
 package bmstu.iu9.lab6.messages;
 
 public class ServerMessage {
+
+    private String[] servers;
+
+    public ServerMessage(String[] servers) {
+        this.servers = servers;
+    }
+
+    public String[] getServers() {
+        return servers;
+    }
+
 }
