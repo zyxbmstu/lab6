dcbcd89084d312952f3b964fafa4e24b9575796a
zyxbmstu <zaxarler@mail.ru>
2021-12-17 12:17:33 +0300

    Scripted auto-commit on change (2021-12-17 12:17:33) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab6/Server.java
+++ src/main/java/bmstu/iu9/lab6/Server.java
@@ -1,4 +1,17 @@
 package bmstu.iu9.lab6;
 
+import akka.actor.ActorRef;
+import akka.http.scaladsl.Http;
+
+import java.io.IOException;
+
 public class Server {
+
+    private Http http;
+    private
+
+    public Server(Http http, int port, ActorRef storageActor) throws IOException {
+        this.http = http;
+    }
+
 }
