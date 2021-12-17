59170d1c906c967eecd70d41510d653522123341
zyxbmstu <zaxarler@mail.ru>
2021-12-17 18:44:10 +0300

    Scripted auto-commit on change (2021-12-17 18:44:10) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab6/Anonymazer.java
+++ src/main/java/bmstu/iu9/lab6/Anonymazer.java
@@ -1,12 +1,15 @@
 package bmstu.iu9.lab6;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
@@ -15,6 +18,7 @@ import akka.stream.javadsl.Flow;
 public class Anonymazer {
 
     private static final String ACTOR_SYSTEM_NAME = "anonymizer";
+    private static final String HOST = "localhost";
 
     public static void main(String[] args) throws IOException {
         int port = Integer.parseInt(args[0]);
@@ -26,7 +30,18 @@ public class Anonymazer {
 
         Server server = new Server(http, port, storageActor);
 
-        final Flow<HttpRequest, HttpResponse, NotUsed> flow = server.
+        final Flow<HttpRequest, HttpResponse, NotUsed> flow = server.createRoute().flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                flow,
+                ConnectHttp.toHost(HOST, port),
+                materializer
+        );
+
+        System.out.println("Server started: " + HOST + ":" + port);
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbind -> system.terminate());
     }
 
 }
