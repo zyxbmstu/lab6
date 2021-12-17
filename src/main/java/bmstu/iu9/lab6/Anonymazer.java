e42f1e484801ed7828fdf3aa844ad8986febf585
zyxbmstu <zaxarler@mail.ru>
2021-12-17 11:34:10 +0300

    Scripted auto-commit on change (2021-12-17 11:34:10) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab6/Anonymazer.java
+++ src/main/java/bmstu/iu9/lab6/Anonymazer.java
@@ -1,7 +1,12 @@
 package bmstu.iu9.lab6;
 
+import java.io.IOException;
+
 public class Anonymazer {
 
-    
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(args[0]);
+        ActorSystem system = Ac
+    }
 
 }
