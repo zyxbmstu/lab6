d001b310d36a32f7d8293b03b54ae6c77df758de
zyxbmstu <zaxarler@mail.ru>
2021-12-17 11:46:37 +0300

    Scripted auto-commit on change (2021-12-17 11:46:37) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab6/StorageActor.java
+++ src/main/java/bmstu/iu9/lab6/StorageActor.java
@@ -2,6 +2,7 @@ package bmstu.iu9.lab6;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+import bmstu.iu9.lab6.messages.RandomServer;
 import bmstu.iu9.lab6.messages.ServerMessage;
 
 public class StorageActor extends AbstractActor {
@@ -12,8 +13,9 @@ public class StorageActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create().match(ServerMessage.class, message -> {
             this.servers = message.getServers();
-        }).match(message ->
-                )
+        }).match(RandomServer.class, message ->
+                sender().tell()
+        ).build();
     }
 
 }
