cbda16547fe5e59761ba55e8e818d7a48546222d
zyxbmstu <zaxarler@mail.ru>
2021-12-17 11:49:23 +0300

    Scripted auto-commit on change (2021-12-17 11:49:23) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab6/StorageActor.java
+++ src/main/java/bmstu/iu9/lab6/StorageActor.java
@@ -5,12 +5,15 @@ import akka.japi.pf.ReceiveBuilder;
 import bmstu.iu9.lab6.messages.RandomServer;
 import bmstu.iu9.lab6.messages.ServerMessage;
 
+import java.util.Random;
+
 public class StorageActor extends AbstractActor {
 
     private String[] servers;
 
     private String getRandomServer() {
         String url = servers[new Random().nextInt(servers.length)];
+        System.out.println("REQUEST REDIRECTED: " + url);
     }
 
     @Override
