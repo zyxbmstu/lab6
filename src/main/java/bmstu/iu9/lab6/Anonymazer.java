1f0999efc9eb1d3e88dab310209f5915567670d2
zyxbmstu <zaxarler@mail.ru>
2021-12-17 11:35:23 +0300

    Scripted auto-commit on change (2021-12-17 11:35:23) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab6/Anonymazer.java
+++ src/main/java/bmstu/iu9/lab6/Anonymazer.java
@@ -1,12 +1,16 @@
 package bmstu.iu9.lab6;
 
 import java.io.IOException;
+import akka.actor.ActorSystem;
 
 public class Anonymazer {
 
+    private static final String ACTOR_SYSTEM_NAME = "anonymizer";
+
     public static void main(String[] args) throws IOException {
         int port = Integer.parseInt(args[0]);
-        ActorSystem system = Ac
+        ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
+        
     }
 
 }
