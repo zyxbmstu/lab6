2d8021560d0d7b8e381ec2a2ac9b7225f6729581
zyxbmstu <zaxarler@mail.ru>
2021-12-17 18:28:08 +0300

    Scripted auto-commit on change (2021-12-17 18:28:08) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab6/Server.java
+++ src/main/java/bmstu/iu9/lab6/Server.java
@@ -2,8 +2,10 @@ package bmstu.iu9.lab6;
 
 import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 import org.apache.zookeeper.KeeperException;
 
 import java.io.IOException;
@@ -43,6 +45,12 @@ public class Server {
                         })))
     }
 
-    private CompletionStage<HttpResponse>
+    private CompletionStage<HttpResponse> fetch(String url) {
+        return http.singleRequest(HttpRequest.create(url));
+    }
+
+    private CompletionStage<HttpResponse> redirect(String url, int count) {
+        return Patterns.ask(storageActor)
+    }
 
 }
